cb47898e03e8a3560fd757fb3df3853db589bd04
Meshano Fransman <shano.fransman@student.rocva.nl>
2019-12-10 15:32:27 +0100

    opdr 3 af

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,10 @@
-//javascript code
+var getal1 = prompt("Wat is uw saldo?");
+if (getal1 == 500){
+  alert ("Er is salaris gestort.")
+}
+if (getal1 >= 501){
+    alert ("Er is salaris en een bonus gestort")
+}
+if (getal1 <= 499){
+    alert ("Er is geen geld gestort")
+}
