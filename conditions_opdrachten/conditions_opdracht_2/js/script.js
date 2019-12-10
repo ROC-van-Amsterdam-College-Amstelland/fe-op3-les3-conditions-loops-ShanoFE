ca6277df9bf324c5ad8109cd6d330679bfc44c44
Meshano Fransman <shano.fransman@student.rocva.nl>
2019-12-10 15:22:55 +0100

    Opdr 2 af

--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,6 +1,6 @@
 var getal1 = prompt("Wat is uw saldo?");
-if (getal1 => 25){
-    document.getElementById("test").style.backgroundColor = "red";
+if (getal1 >= 25){
+  prompt ("Je hebt voldoende saldo om 25 euro op te nemen.")
 } else {
-
+  prompt ("Sorry, je hebt te weinig saldo")
 }
